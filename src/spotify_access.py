25c3f230bcd01a1a9188c4b009e4984acda1a190
steff <98240487+a-asen@users.noreply.github.com>
2023-02-23 17:54:48 +0100

    small work

--- src/spotify_access.py
+++ src/spotify_access.py
@@ -6,8 +6,12 @@ Created on Thu Feb 16 12:46:58 2023
 """
 #%% 
 import os
+import json
+import pandas as pd
 
-os.chdir("D:/coding/GitHub/a-asen/spotify_project") # set wprlomg dorectpry
+os.chdir("D:/coding/GitHub/a-asen/spotify_project") # Change working directory: Laptop 
+os.chdir("D:/coding/GitHub/a-asen/spotify_project") # Change working directory: Home
+
 #%%
 #access_token  
 
@@ -15,13 +19,17 @@ os.chdir("D:/coding/GitHub/a-asen/spotify_project") # set wprlomg dorectpry
 os.path.exists("access")
 print(os.getcwd())
 
-import json
-#%%
+
+#%%    Get access token
 with open("access/access_token.json", "r") as f:
     access_token = json.load(f)
+    
+test = pd.read_json("access/access_token.json")
 
 client = access_token["Client_ID"]
-secret = access_token["Client "]
+secret = access_token["Client"]
+
+
 
 #%%
 endpoint = "https://api.spotify.com"
@@ -30,6 +38,7 @@ endpoint = "https://api.spotify.com"
 import requests
 
 url = "https://api.spotify.com/v1/me"
+
 headers = {
     "Authorization": "<your_access_token>",
     "Content-Type": "application/json"
@@ -37,3 +46,26 @@ headers = {
 response = requests.get(url, headers=headers)
 
 print(response.json())
+
+#%%%
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id = client,
+                                                           client_secret = secret))
+
+results = sp.search(q='weezer', limit=20)
+for idx, track in enumerate(results['tracks']['items']):
+    print(idx, track['name'])
+
+
+
+
+
+
+
+
+
+
+
+
