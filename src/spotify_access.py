b006c2d19b19bb1197024e4e35e8f1b3e2287d66
steff <98240487+a-asen@users.noreply.github.com>
2023-02-28 17:36:45 +0100

    base work V2

--- src/spotify_access.py
+++ src/spotify_access.py
@@ -15,8 +15,8 @@ from spotipy.oauth2 import SpotifyClientCredentials
 
 if os.path.exists("D:/coding/GitHub/a-asen/spotify_project"): # School 
     path = "D:/coding/GitHub/a-asen/spotify_project"
-if os.path.exists("D:/_coding/GitHub/a-asen/spotify_project"): # Home
-    path = "D:/_coding/GitHub/a-asen/spotify_project"
+#if os.path.exists("D:/_coding/GitHub/a-asen/spotify_project"): # Home
+ #   path = "D:/_coding/GitHub/a-asen/spotify_project"
     
 os.chdir(path)
 print(os.getcwd())
@@ -35,52 +35,69 @@ sp_secret = access_token["Client_Secret"]
 sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id = sp_client,
                                                            client_secret = sp_secret))
 
-#%% Read my user
+#%% Get user. 
 my_user = sp.user_playlists("1117238547")
 
-#%%  Read my playlist 
+my_user.keys()
+my_user["items"][1].keys()
+
+my_user["items"][1]
+
+#%%
+for row in my_user["items"]:
+    print(row["name"])
+    print(row["id"])
+
+
+#%%  
 # Get my playlist 
 my_playlist = sp.playlist("5QYCZkI9ecneIK5OjveBsE")  # 2023 - MixTape
 # dat = json.dumps(my_playlist) # save to file
 
-# Get "dict" keys
-my_playlist.keys()
-    # 'collaborative', 'description', 'external_urls', 'followers', 'href', 'id', 'images', 
-    # 'name', 'owner', 'primary_color', 'public', 'snapshot_id', 'tracks', 'type', 'uri'
-        # "tracks" most useful    # my_playlist["tracks"]
+for enu, data in enumerate(my_playlist["tracks"]["items"]):
+   
+    print(enu)
+   
+    d = {} # New dictinary (to add playlist information)
+    artist_list = [] # List to get out "multiple" artits
+    
+    # Track information
+    d["number"]       = data["track"]["track_number"]    # Track number in the playlist 
+    d["name"]         = data["track"]["name"]            # Name of the song
+    
+    for artist in data["track"]["artists"]:
+        artist_list.append(artist["name"])
+    d["aritsts"]      = artist_list
+    d["artists"]      = data["track"]["artists"][""]
+    d["popularity"]   = data["track"]["popularity"]      # How popular the song is
+    d["duration"]     = data["track"]["duration_ms"]     # How long the song is 
+    
+    # Meta-data
+    d["id"]           = data["track"]["id"]              # Id of the track 
+    d["added_date"]   = data["added_at"]                 # The date at which the song was added to the playlist
+    d["added_by"]     = data["added_by"]["id"]           # The person who added the song to the playlist
+    data["track"]["is_local"]
+    data["track"].keys()
+    data["track"]["name"]
+    data["track"]["name"]
+    data["track"]["name"]
+    
 
-# Get "dict" keys under "tracks"
-my_playlist["tracks"].keys()
-    # 'href', 'items', 'limit', 'next', 'offset', 'previous', 'total'
-        # "items" and "total" useful    # my_playlist["tracks"]["items"]
-
-# Get dict keys under "items" under "tracks" 
-    # my_playlist["tracks"]["items"].keys() 
-    len(my_playlist["tracks"]["items"])
-
-# Under "tracks","items" we have a list. Access respectively with "[0]" 
-my_playlist["tracks"]["items"][0].keys()
-    # 'added_at', 'added_by', 'is_local', 'primary_color', 'track', 'video_thumbnail'
-    # "track" most useful 
-
-# under "tracks" under any respective item under "items" under "tracks"
-my_playlist["tracks"]["items"][0]["track"].keys()
-    # 'album', 'artists', 'available_markets', 'disc_number', 'duration_ms', 'episode', 'explicit', 'external_ids', 
-    # 'external_urls', 'href', 'id', 'is_local', 'name', 'popularity', 'preview_url', 'track', 'track_number', 'type', 'uri'
+    print(data[enu]["track"])
+    for y in x["track"]:
+        y.keys()
+    
+    if "track" in x:
+        print(x)
+        for y in x["track"]:
+            print(y)
+            # print(y["uri"])
+            # print(y["name"])
 
 
-my_playlist["tracks"]["items"][0]["track"]["uri"]
-
-my_playlist["tracks"]["items"][0]["track"]["track_number"]
-my_playlist["tracks"]["items"][0]["track"]["name"]
-
-my_playlist["tracks"]["items"][0]["track"]["popularity"]
-my_playlist["tracks"]["items"][0]["track"]["duration_ms"]
-my_playlist["tracks"]["items"][0]["track"]["track"]
-
-
-my_playlist["tracks"]["items"]#[0]#["track"]
-["name"]
+#%%%      ####### AUDIO FEATURE
+audio_feature.keys()
+audio_feature = sp.audio_analysis('spotify:track:0o3G5EAvMwjzqX5cH55Kw5')
 
 
 
